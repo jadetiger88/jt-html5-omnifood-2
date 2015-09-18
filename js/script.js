b224f43c9567326556a0cc339542f6db301ec630
JadeTiger88 <jadetiger88@gmail.com>
2015-09-18 16:43:18 -0700

    Add mobile nav bar

--- js/script.js
+++ js/script.js
@@ -17,13 +17,41 @@ $(document).ready(function() {
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 	      if (target.length) {
 	        $('html,body').animate({
-	          scrollTop: target.offset().top
+	          scrollTop: target.offset().top - 60
 	        }, 1000);
 	        return false;
 	      }
 	    }
 	  });
 	});
+
+	$("#main-nav i").click(function() {
+		var nav = $("#main-nav ul li"); 
+		var icon = $("#main-nav i"); 
+		nav.slideToggle("slow");
+
+		if (icon.hasClass("fa-bars")) {
+			icon.removeClass("fa-bars"); 
+			icon.addClass("fa-times-circle"); 
+		} else {
+			icon.addClass("fa-bars"); 
+			icon.removeClass("fa-times-circle"); 
+		}
+	});
+
+	// $("#main-nav li a").click(function() {
+	// 	var nav = $("#main-nav ul li"); 
+	// 	var icon = $("#main-nav i"); 
+	// 	nav.slideToggle("slow");
+
+	// 	if (icon.hasClass("fa-bars")) {
+	// 		icon.removeClass("fa-bars"); 
+	// 		icon.addClass("fa-times-circle"); 
+	// 	} else {
+	// 		icon.addClass("fa-bars"); 
+	// 		icon.removeClass("fa-times-circle"); 
+	// 	}
+	// });
 
 
 }); 
